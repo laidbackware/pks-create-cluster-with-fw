4d0a7d582aceecc4a8af52b577032188d7a637a0
Matt Proud <proudmatt@gmail.com>
2019-07-14 20:33:12 +0100

    update script and template

--- pks_create_cluster.sh
+++ pks_create_cluster.sh
@@ -9,15 +9,16 @@ then
   exit 1
 fi
 
-pks create-cluster ${CLUSTER_NAME} --external-hostname ${CLUSTER_NAME} --plan small
-CLUSTER_ID=$(pks cluster ${CLUSTER_NAME} --json |jq -rc '.uuid')
-echo "Created cluster with ID ${CLUSTER_ID}"
+pks create-cluster ${CLUSTER_NAME} --external-hostname ${CLUSTER_NAME}.home.local --plan small
 
-export ANSIBLE_LIBRARY="${PWD}/ansible-for-nsxt"
-export ANSIBLE_MODULE_UTILS="${PWD}/ansible-for-nsxt/module_utils"
+CLUSTER_ID=$(pks cluster ${CLUSTER_NAME} --json |jq -rc '.uuid')
+
+echo "Created cluster with ID ${CLUSTER_ID}"
 
 CLUSTER_RULES_FILE=$"cluster_firewall_rules_${CLUSTER_ID}.yml"
 cp template_cluster_firewall_rules.yml ${CLUSTER_RULES_FILE}
 sed -i "s/<cluster_id>/${CLUSTER_ID}/g" ${CLUSTER_RULES_FILE}
 
+export ANSIBLE_LIBRARY="${PWD}/ansible-for-nsxt"
+export ANSIBLE_MODULE_UTILS="${PWD}/ansible-for-nsxt/module_utils"
 ansible-playbook nsgroup_firewall_section.yml --extra-vars="@${CLUSTER_RULES_FILE}" #-vvv
